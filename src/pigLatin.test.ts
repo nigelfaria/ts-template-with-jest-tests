b733aaa3897c3366f67db955861a0368c12ead2e
Nigel Faria <nigel.faria@gmail.com>
2023-01-04 13:02:11 +0000

    Test case 2

--- src/pigLatin.test.ts
+++ src/pigLatin.test.ts
@@ -6,4 +6,8 @@ describe("test pig Latin function ", () => {
   it("should return igPay atinlay siay oolcay for pigIt('Pig latin is cool')", () => {
     expect(pigIt("Pig latin is cool")).toBe("igPay atinlay siay oolcay");
   });
+  it("should return igPay atinlay siay oolcay for pigIt('Hello world !')", () => {
+    expect(pigIt("Hello world !")).toBe("elloHay orldway !");
+  });
+
 });
